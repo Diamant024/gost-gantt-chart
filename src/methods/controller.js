915a16e16da7c805788b988ebe6fa358c94ee56d
Kirill <d-iamant@yandex.ru>
2021-02-10 10:11:30 +0300

    Style fixes, QS adaptation (in progress)

--- src/methods/controller.js
+++ src/methods/controller.js
@@ -1,7 +1,64 @@
+let qlik = window.require('qlik');
+
 export default [
 	"$scope",
 	"$element",
 	function($scope, $element) {
-		// ..controller code here
-	},
+
+	console.log($scope.layout.tasks)
+
+		let app = qlik.currApp();
+
+		$scope.hyperCube;
+
+		function createHyperCube() {
+			if ($scope.hyperCube) {
+				console.log('existing cube', $scope.hyperCube);
+				app.destroySessionObject($scope.hyperCube.qInfo.qId).then(function (reply) {
+					console.log('destroySessionObject result', reply);
+				});
+			}
+
+			if ($scope.layout.props && !_.isEmpty( $scope.layout.props.dimension1 ) && !_.isEmpty( $scope.layout.props.measure1 ) ) {
+
+				let cubeDef = {
+					qDimensions: [{
+						qDef: {
+							qFieldDefs: [$scope.layout.props.dimension1],
+							qSortCriterias: [
+								{
+									qSortByAscii: 1
+								}
+							]
+						}
+					}],
+					qMeasures: [{
+						qDef: {
+							qDef: "=" + $scope.layout.props.measure1
+						}
+					}],
+					qInterColumnSortOrder: [0, 1],
+					qInitialDataFetch: [
+						{
+							qTop: 0,
+							qLeft: 0,
+							qHeight: 100,
+							qWidth: 2
+						}
+					]
+				};
+
+				app.createCube(cubeDef, function (reply) {
+					console.log('cube', reply);
+					$scope.hyperCube = reply;
+				});
+			}
+		}
+
+		$scope.$watchCollection('layout.props', function ( newVal ) {
+			console.log( 'new Vals', newVal );
+			createHyperCube();
+		});
+		createHyperCube();
+	}
 ]
--- src/methods/definition.js
+++ src/methods/definition.js
@@ -2,15 +2,63 @@ export default {
 	type: "items",
 	component: "accordion",
 	items: {
-		appearancePanel: {
+		tasksData: {
+			label: "Задачи",
+			ref: "tasks",
+			items: {
+				dim1: {
+					ref: "tasks.dimension1",
+					label: "ID задачи",
+					type: "string",
+					component: "expression",
+					expression: "optional"
+				},
+				dim2: {
+					ref: "tasks.dimension2",
+					label: "Название задачи",
+					type: "string",
+					component: "expression",
+					expression: "optional"
+				},
+				mea1: {
+					ref: "tasks.measure1",
+					label: "Measure",
+					type: "string",
+					component: "expression",
+					expression: "optional"
+				}
+			},
+		},
+		relationsData: {
+			label: "Связи",
+			ref: "relations",
+			items: {
+				dim1: {
+					ref: "relations.dimension1",
+					label: "ID потомка",
+					type: "string",
+					component: "expression",
+					expression: "optional"
+				},
+				dim2: {
+					ref: "relations.dimension2",
+					label: "ID родителя",
+					type: "string",
+					component: "expression",
+					expression: "optional"
+				},
+				mea1: {
+					ref: "relations.measure1",
+					label: "Тип связи",
+					type: "string",
+					component: "expression",
+					expression: "optional"
+				}
+			},
+		},
+		appearance: {
 			uses: "settings",
 			items: {
-				baseURL: {
-					ref: "baseURL",
-					type: "string",
-					label: "API base url",
-					defaultValue: ""
-				}
 			}
 		}
 	},
